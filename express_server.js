7932ce57d964b21c1452f60f9ca53db0a7e29665
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-21 14:18:58 -0400

    Modify express_server to see changes with 'get' and 'send' using express

--- express_server.js
+++ express_server.js
@@ -15,9 +15,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get("/hell", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
