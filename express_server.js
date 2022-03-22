3e328cc6a7dd8a1a02ff9616cf13d9600c3c0e18
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-21 21:36:49 -0400

    created a redirect to the longURL- main site

--- express_server.js
+++ express_server.js
@@ -5,9 +5,9 @@ app.set("view engine", "ejs"); // Setting ejs as the view engine
 const bodyParser = require("body-parser"); // imported installed package (middleware) body-parser
 app.use(bodyParser.urlencoded({extended: true})); // setting app to use bodyParser created.
 
-function generateRandomString() {
+const generateRandomString = function() {
   return Math.random().toString(36).substring(2,8);
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,12 +18,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to my home page!");
 });
 
-let shortURL = "";
 app.post("/urls", (req, res) => {
-  shortURL = generateRandomString();
+  const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL; // set the post request body to a random string six string.
-  res.redirect(`/urls/${shortURL}`)         // Respond with a  redirect URL using the generated shortURL-longURL pair
+  res.redirect(`/u/${shortURL}`);       // Respond with a  redirect URL using the generated shortURL-longURL pair
 });
+
 
 
 
@@ -42,11 +42,15 @@ app.get("/urls/new", (req, res) => {
 
 // Shows a single shortURL with corresponding longURL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; 
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-
+//  create a redirect to the longURL, main website.
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 
 app.listen(PORT, () => {
